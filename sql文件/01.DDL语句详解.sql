164ebe57325aeb6e24a1a0860b06d1eecc3fdaf1
mingyue12 <108075525+mingyue12@users.noreply.github.com>
2026-03-20 00:22:00 +0800

    Update 01.DDL语句详解.sql
    yes

--- sql文件/01.DDL语句详解.sql
+++ sql文件/01.DDL语句详解.sql
@@ -17,13 +17,47 @@ SQL语句介绍：
                     # 单行注释
                     -- 单行注释
 
+数据类型：
+    概述：
+        就是用来限定某列值的范围的，必须是：整数，小数，字符串，日期等...
+    常用的数据类型：
+        整型：      int, smallint, mediumint, bigint
+        浮点型：    double, float, decimal(精度,小数位数)
+        日期型：    datetime
+        时间型：    time
+        字符串型：   varchar(长度),char(长度)
+
+
+约束：
+        就是用来限定某列值的范围的，必须是：整数，小数，字符串，日期等...
+    常用的约束：
+        not null：不能为空
+        unique：唯一
+        primary key：主键
+        foreign key：外键
+        check：检查
+        default：默认值
+        comment：注释
+    概述：
+        在数据类型的基础上，进一步对该值做限定。
+    例如常用分类：
+        单表约束：
+            primary key：主键约束，特点：不能为空，唯一，不能重复
+            not null：   非空约束，即该列值不能为空，但是可以重复
+            unique：     唯一约束，即该列值必须是唯一的，不能重复
+            default：    默认值约束，即该列值默认值为指定值，不指定时，为null
+        多表约束：
+            foreign key：外键约束，特点：不能为空，必须是其他表的主键值
+
+
+
 */
 
 # 1.查看本机安装的所有数据库
 show DATABASES;
 SELECT database();
 
-# 案例一：DDL语句操作数据库
+# ----------------------------------------案例一：DDL语句操作数据库----------------------------------- #
 # 1.查看已创建的所有数据库
 show DATABASES;
 
@@ -51,3 +85,40 @@ use day01;
 # 7.查看某个已安装的数据库的码表
 show create DATABASE day01; # 默认utf-8码表
 show create DATABASE day02; # 使用utf-8码表创建
+
+
+# ----------------------------------------案例二：DDL语句操作数据表（table）----------------------------------- #
+# 1.切库
+use day01;
+
+# 2.查看（当前数据库中）所有的数据表
+show TABLES;
+
+# 3.创建数据表，学生表：student，字段为：sid， 学生id, name,学生姓名，age，学生年龄
+/*
+格式：
+    create table [if not exists] student(
+        字段名 数据类型 [约束]，
+        字段名 数据类型 [约束]，
+        字段名 数据类型 [约束]
+    );
+ */
+
+Create table if not exists student(
+    sid int,            # 学生学号
+    name varchar(20),   # 学生姓名
+    age int             # 学生年龄
+);
+
+# 4.修改数据表名称 student -> stu
+# 格式： rename table student to stu
+rename table student to stu;
+
+# 5.删除数据表
+# 格式： drop table stu;
+drop table if exists stu;
+
+
+# 6.查看表结构
+# 格式：desc 数据表名称
+DESC stu;
